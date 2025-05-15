4438b43c503e15f1c5ecd095e68e3c837098bd4b
SantiagoVarga <santi.varga03@gmail.com>
2025-05-15 19:01:15 -0300

    Snapshot y jugadores BASICO (solo posicion)

--- client_src/client_dibujador.cpp
+++ client_src/client_dibujador.cpp
@@ -6,17 +6,25 @@ Dibujador::Dibujador(Queue<Snapshot>& cola_snapshots) : cola_snapshots(cola_snap
 
 void Dibujador::renderizar(Renderer &renderer, bool &jugador_activo)
 {
-    //Snapshot snapshotActual;
-    //while (cola_snapshots.try_pop(snapshotActual)) {
-      //  this->ultimo_estado_recibido = estado_actual;
-    //}
- 
-    Texture fondo(renderer, Surface("client_src/gfx/backgrounds/dust.png"));
-    renderer.Clear();
-    renderer.Copy(fondo,NullOpt,Rect(0, 0, ANCHO_MIN, ALTO_MIN));
-    renderer.Present();
-   
-}   
+  Snapshot snapshotActual;
+  while (cola_snapshots.try_pop(snapshotActual)) {
+    this->ultimo_estado_recibido = snapshotActual;
+  }
+
+  Texture fondo(renderer, Surface("client_src/gfx/backgrounds/dust.png"));
+  renderer.Clear();
+  renderer.Copy(fondo, NullOpt, Rect(0, 0, ANCHO_MIN, ALTO_MIN));
+
+  
+  for (const auto& jugador : this->ultimo_estado_recibido.jugadores) {
+    int x = jugador.getX(); 
+    int y = jugador.getY(); 
+    Texture jugadorTexture(renderer, Surface("client_src/gfx/sprites/player.png"));
+    renderer.Copy(jugadorTexture, NullOpt, Rect(x, y, TAM_JUGADOR, TAM_JUGADOR));
+  }
+
+  renderer.Present();
+}
 
 Dibujador::~Dibujador()
 {
